d3a14f1084903a7d0590ac52cbd3084c1925849a
Seung@gram-win <ncraig@naver.com>
2019-04-22 14:36:21 +0900

    motor 2x

---
+++ Motor/02_HelloDCMotor_2/02_HelloDCMotor_2.ino
@@ -0,0 +1,46 @@
+// https://create.arduino.cc/projecthub/remnis/h-bridge-for-your-robot-025ae8
+
+int IN1 = 10;
+int IN2 = 11;
+
+int IN3 = 5;
+int IN4 = 6;
+
+void setup() {
+  Serial.begin(9600);
+  pinMode(LED_BUILTIN, OUTPUT);  
+  pinMode(IN1, OUTPUT);    
+  pinMode(IN2, OUTPUT);
+  pinMode(IN3, OUTPUT);    
+  pinMode(IN4, OUTPUT);  
+}
+ 
+void loop() {
+  // forward
+  digitalWrite(IN1, HIGH);
+  digitalWrite(IN2, LOW);
+  digitalWrite(IN3, HIGH);
+  digitalWrite(IN4, LOW);
+  digitalWrite(LED_BUILTIN, HIGH);
+  Serial.print('1');
+  delay(1000);
+
+   // backward
+  digitalWrite(IN1, LOW);
+  digitalWrite(IN2, HIGH);
+  digitalWrite(IN3, LOW);
+  digitalWrite(IN4, HIGH);
+
+  digitalWrite(LED_BUILTIN, LOW);
+  Serial.print('2');
+  delay(1000);
+
+  digitalWrite(IN1, HIGH);
+  digitalWrite(IN2, HIGH);
+  digitalWrite(IN3, HIGH);
+  digitalWrite(IN4, HIGH);  
+  digitalWrite(LED_BUILTIN, HIGH);
+  Serial.print('0');
+  delay(1000);
+  
+}
