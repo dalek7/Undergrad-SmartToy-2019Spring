fa09f333223bb4276e1335110e13c608484cf848
Seung@MBP linc <ncraig@naver.com>
2019-04-29 14:08:29 +0900

    pwm

---
+++ Motor/03_DCMotorHelloPWM/03_DCMotorHelloPWM.ino
@@ -0,0 +1,73 @@
+// https://create.arduino.cc/projecthub/remnis/h-bridge-for-your-robot-025ae8
+// https://howtomechatronics.com/tutorials/arduino/arduino-dc-motor-control-tutorial-l298n-pwm-h-bridge/
+
+int IN1 = 9;
+int IN2 = 10;
+int enA = 11;
+
+int motorSpeedA = 0;
+
+void setup() {
+  Serial.begin(9600);
+  pinMode(LED_BUILTIN, OUTPUT);  
+  
+  pinMode(enA, OUTPUT);
+  pinMode(IN1, OUTPUT);    
+  pinMode(IN2, OUTPUT);
+
+
+  // Set initial rotation direction
+  // forward
+  digitalWrite(IN1, HIGH);
+  digitalWrite(IN2, LOW);
+
+  // Initial speed
+  motorSpeedA = 100; // 0~255
+  
+}
+ 
+void loop() {
+  
+  
+  if (motorSpeedA < 0) 
+  {
+    motorSpeedA = 0;
+  }
+  if (motorSpeedA > 255) 
+  {
+    motorSpeedA = 255;
+  }
+    
+  analogWrite(enA, motorSpeedA); // Send PWM signal to motor A
+}
+
+
+
+void serialEvent() {
+  while (Serial.available()) 
+  {
+    // get the new byte:
+    char inChar = (char)Serial.read();
+    
+    if(inChar == '0')
+    {
+      digitalWrite(IN1, LOW);
+      digitalWrite(IN2, LOW);
+      
+    }
+    else if(inChar == 'f')
+    {
+      digitalWrite(IN1, HIGH);
+      digitalWrite(IN2, LOW);
+      
+    }
+    else if(inChar == 'b')
+    {
+      digitalWrite(IN1, LOW);
+      digitalWrite(IN2, HIGH);
+      
+    }
+  }
+}
+
+
---
+++ Motor/03_DCMotorHelloPWM/readme.md
@@ -0,0 +1,5 @@
+### About PWM
+* 펄스 폭 변조 또는 PWM(Pulse Width Modulation)방식은 펄스의 폭을 조절하는 방식으로 변조한다.
+
+<img src='PWM_duty_cycle_with_label.gif' />
+
