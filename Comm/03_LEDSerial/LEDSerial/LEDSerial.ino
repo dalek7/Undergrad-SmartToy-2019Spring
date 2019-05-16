547f5b8cd49ae5d24bafc65c86519b3ad6e98837
Seung@gram-win <ncraig@naver.com>
2019-05-16 15:40:23 +0900

    sync

--- Comm/03_LEDSerial/LEDSerial/LEDSerial.ino
+++ Comm/03_LEDSerial/LEDSerial/LEDSerial.ino
@@ -1,4 +1,3 @@
-
 void setup() {
   Serial.begin(9600);    // opens serial port, sets data rate to 9600 bps
   pinMode(LED_BUILTIN, OUTPUT);
@@ -6,18 +5,15 @@ void setup() {
 
 void loop() {
   if (Serial.available() > 0) {
-
     int incomingByte = Serial.read();
     if(incomingByte == '1')
     {
-      // turn the LED on (HIGH is the voltage level)
-      digitalWrite(LED_BUILTIN, HIGH);   
+      digitalWrite(LED_BUILTIN, HIGH);//(HIGH is the voltage level)
       Serial.print("On");
     }
     else if(incomingByte == '0')
     {
-      // turn the LED off by making the voltage LOW
-      digitalWrite(LED_BUILTIN, LOW);    
+      digitalWrite(LED_BUILTIN, LOW); //making the voltage LOW
       Serial.print("Off");
     }
   }
