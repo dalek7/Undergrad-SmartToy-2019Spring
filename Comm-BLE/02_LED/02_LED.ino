7517cd081a135bccb52e7e09ff4e38a4a4369ad6
Seung@gram-win <ncraig@naver.com>
2019-05-20 15:54:01 +0900

    added Serial.print

--- Comm-BLE/02_LED/02_LED.ino
+++ Comm-BLE/02_LED/02_LED.ino
@@ -4,6 +4,7 @@ SoftwareSerial BTSerial(4, 5);
 void setup() {
   pinMode(LED_BUILTIN, OUTPUT);
   BTSerial.begin(9600);
+  Serial.begin(9600);
 }
 
 void loop() {
@@ -13,10 +14,12 @@ void loop() {
     if(incomingByte == '1')
     {
       digitalWrite(LED_BUILTIN, HIGH);
+      Serial.println("ON");
     }
     else if(incomingByte == '0')
     {
       digitalWrite(LED_BUILTIN, LOW); 
+      Serial.println("OFF");
     }
   }
 }
